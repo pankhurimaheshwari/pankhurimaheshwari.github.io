2a4385db49346c9e1aaa7967cbd5a6ef7333ef1b
Pankhuri and Kaushal <pankhuriandkaushal@Pankhuris-MacBook-Air.local>
2025-03-04 17:36:33 +0530

    Updated vite.config.ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/pankhurimaheshwari.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
